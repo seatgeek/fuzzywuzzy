ac5b87d8cde55a815633d46d82455a0edaa633ac
Adam Cohen <adam@seatgeek.com>
2011-07-08 15:42:23 -0400

    distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fuzzywuzzy',
+    version='0.1',
+    description='Fuzzy string matching in python',
+    author='Adam Cohen',
+    author_email='adam@seatgeek.com',
+    url='https://github.com/seatgeek/fuzzywuzzy/',
+    packages=['fuzzywuzzy'])
