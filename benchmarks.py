c5fdad53f861e0a378461f124ec8a77b1084ef37
Tristan Launay <tlaunay@work4labs.com>
2013-03-20 14:56:44 +0000

    Proper benchmark display. Introduce methods to explicitly do all the unicode preprocessing *before* using fuzz lib.

--- benchmarks.py
+++ benchmarks.py
@@ -4,7 +4,7 @@ from timeit import timeit
 from fuzzywuzzy import utils
 import math
 
-iterations=100000
+iterations = 1000
 
 cirque_strings = [
     "cirque du soleil - zarkana - las vegas",
@@ -34,38 +34,43 @@ mixed_strings = [
     u"a\xac\u1234\u20ac\U00008000"
     ]
 
+common_setup = "from fuzzywuzzy import fuzz, utils; "
+basic_setup = "from fuzzywuzzy.string_processing import StringProcessor; StringProcessor.ensure_chars_from_categories_regex_exists('LNZ')"
+
 def print_result_from_timeit(stmt='pass', setup='pass', number=1000000):
     """
     Clean function to know how much time took the execution of one statement
     """
-    units     = ["s", "ms", "us", "ns"]
-    thousands = int(math.log(number, 1000))
+    units           = ["s", "ms", "us", "ns"]
+    duration        = timeit(stmt, setup, number=number)
+    avg_duration    = duration/float(number)
+    thousands       = int(math.floor(math.log(avg_duration, 1000)))
 
-    print "Average took %f %s" % (round(timeit(stmt, setup, number=number),4) * (1000**thousands) / number, units[thousands])
+    print "Total time: %fs. Average run: %.3f%s." % (duration, avg_duration * (1000**-thousands), units[-thousands])
 
 for s in choices:
     print 'Test validate_string for: "%s"' % s
-    print_result_from_timeit('utils.validate_string(\'%s\')' % s, "from fuzzywuzzy import utils", number=iterations)
+    print_result_from_timeit('utils.validate_string(\'%s\')' % s, common_setup, number=iterations)
 
 print
 
 for s in mixed_strings+cirque_strings+choices:
     print 'Test full_process for: "%s"' % s
-    print_result_from_timeit('utils.full_process(u\'%s\')' % s, "from fuzzywuzzy import utils",number=iterations)
+    print_result_from_timeit('utils.full_process(u\'%s\')' % s, common_setup + basic_setup, number=iterations)
 
 ### benchmarking the core matching methods...
 
 for s in cirque_strings:
     print 'Test fuzz.ratio for string: "%s"' % s
     print '-------------------------------'
-    print_result_from_timeit('fuzz.ratio(u\'cirque du soleil\', u\'%s\')' % s, "from fuzzywuzzy import fuzz",number=iterations/100)
+    print_result_from_timeit('fuzz.ratio(u\'cirque du soleil\', u\'%s\')' % s, common_setup + basic_setup, number=iterations/100)
 
 for s in cirque_strings:
     print 'Test fuzz.partial_ratio for string: "%s"' % s
     print '-------------------------------'
-    print_result_from_timeit('fuzz.partial_ratio(u\'cirque du soleil\', u\'%s\')' % s, "from fuzzywuzzy import fuzz",number=iterations/100)
+    print_result_from_timeit('fuzz.partial_ratio(u\'cirque du soleil\', u\'%s\')' % s, common_setup + basic_setup, number=iterations/100)
 
 for s in cirque_strings:
     print 'Test fuzz.WRatio for string: "%s"' % s
     print '-------------------------------'
-    print_result_from_timeit('fuzz.WRatio(u\'cirque du soleil\', u\'%s\')' % s, "from fuzzywuzzy import fuzz",number=iterations/100)
+    print_result_from_timeit('fuzz.WRatio(u\'cirque du soleil\', u\'%s\')' % s, common_setup + basic_setup, number=iterations/100)
--- fuzzywuzzy/string_processing.py
+++ fuzzywuzzy/string_processing.py
@@ -9,31 +9,66 @@ class StringProcessor(object):
     for both input and output.
     """
 
+    MAX_UNICODE = 0x10ffff
+
     # For more information about unicode character categories,
     # check out: http://www.unicode.org/reports/tr44/tr44-4.html#General_Category_Values
-    all_chars       = None
 
-    char_categories = "LMNPSZC" # /L/etter /M/ark /N/umber /P/unctuation /S/ymbol /Z/eparator /C/ontrol
+    char_categories = "LMNPSZC"  # /L/etter /M/ark /N/umber /P/unctuation /S/ymbol /Z/eparator /C/ontrol
     char_family     = {}
 
     replace_separators_with_whitespace_regex = None
     chars_from_categories_regex = {}
-    words_from_categories_regex = {}
 
     @classmethod
-    def get_all_chars(cls):
-        if not cls.all_chars:
-            cls.all_chars = u"".join(unichr(i) for i in xrange(0x10ffff))
-        return cls.all_chars
+    def ensure_char_family_exists(cls, categories):
+        """
+        Build a dict of the form category: "characters in the category".
+        The + [u"\\\\" if category == "P" else u""] part is needed because we need
+        to escape backslash manually in the punctuation category.
+        """
+
+        for category in categories:
+            if category not in cls.char_family and category in cls.char_categories:
+                cls.char_family[category] = u"".join([unichr(i) for i in xrange(cls.MAX_UNICODE) if unicodedata.category(unichr(i))[0]==category] + \
+                                                     [u"\\\\" if category == "P" else u""])
+
+    @classmethod
+    def ensure_chars_from_categories_regex_exists(cls, categories):
+        """
+        Build a dict of the form categories: regex_to_find_chars_from_categories.
+        If categories is a set, a tuple, or a list, it is iterated over.
+        Returns a unicode or a list of unicode depending on the input,
+        where each unicode is a (sorted) key in cls.chars_from_categories_regex.
+        """
+        if isinstance(categories, set) or isinstance(categories, tuple) or isinstance(categories, list):
+            sorted_cats = []
+            needed_cats = set()
+            for cats in categories:
+                # Sort and check the existence of categories.
+                sorted_cats.append(u"".join(sorted([cat for cat in cats if cat in cls.char_categories])))
+                # Least common multiple approach.
+                needed_cats = needed_cats.union(set([cat for cat in sorted_cats]))
+            result = sorted_cats
+        else:
+            sorted_cats = [u"".join(sorted([cat for cat in categories if cat in cls.char_categories]))]
+            needed_cats = set([cat for cat in sorted_cats])
+            result = sorted_cats[0]
+
+        # Make sure char_family is filled in with all that is needed.
+        cls.ensure_char_family_exists(needed_cats)
+
+        # Now make sure cls.chars_from_categories_regex is filled in with all that is needed.
+        for sorted_cat in sorted_cats:
+            if sorted_cat not in cls.chars_from_categories_regex:
+                # Chars of interests.
+                cls.chars_from_categories_regex[sorted_cat] = re.compile("[" + u"".join([cls.get_char_family(cat) for cat in sorted_cat]) + "]")
+
+        return result
 
     @classmethod
     def get_char_family(cls, category):
-        if not category in cls.char_family:
-            # Build a dict of the form category: "characters in the category".
-            # The + [u"\\\\" if category == "P" else u""] part is needed because we need
-            # to escape backslash manually in the punctuation category.
-            cls.char_family[category] = u"".join([char for char in cls.get_all_chars() if unicodedata.category(char)[0]==category] + \
-                                                 [u"\\\\" if category == "P" else u""])
+        cls.ensure_char_family_exists(category)
         return cls.char_family[category]
 
     @classmethod
@@ -47,43 +82,12 @@ class StringProcessor(object):
         return cls.replace_separators_with_whitespace_regex.sub(u" ", a_string)
 
     @classmethod
-    def strip_chars_from_categories(cls, a_string, categories):
-        """
-        This function strips characters from categories down in the string given.
-        """
-        if not categories in cls.chars_from_categories_regex:
-            # Chars of interests.
-            cls.chars_from_categories_regex[categories] = \
-                re.compile("[" + u"".join([cls.get_char_family(category) for category in categories]) + "]")
-
-        return cls.chars_from_categories_regex[categories].sub(u"", a_string)
-
-    @classmethod
     def keep_only_chars_from_categories(cls, a_string, categories):
         """
         This function keeps only characters from categories it finds in the string given.
         """
-        if not categories in cls.chars_from_categories_regex:
-            # Chars of interests.
-            cls.chars_from_categories_regex[categories] = \
-                re.compile("[" + u"".join([cls.get_char_family(category) for category in categories]) + "]")
-
-        return u"".join(cls.chars_from_categories_regex[categories].findall(a_string))
-
-
-    @classmethod
-    def strip_words_from_categories(cls, a_string, categories):
-        """
-        This function strips words whose characters belong to categories in the string given.
-        """
-        if not categories in cls.words_from_categories_regex:
-            # Separator or start of string + Chars of interests (possibly repeated) + Separator or end of string.
-            cls.words_from_categories_regex[categories] = \
-                re.compile("((?<=[" + cls.get_char_family("Z") +"])|^)" + \
-                           "[" + u"".join([cls.get_char_family(category) for category in categories]) + "]+" + \
-                           "((?=[" + cls.get_char_family("Z") + "])|$)")
-
-        return cls.words_from_categories_regex[categories].sub(u"", a_string)
+        cat_key = cls.ensure_chars_from_categories_regex_exists(categories)
+        return u"".join(cls.chars_from_categories_regex[cat_key].findall(a_string))
 
     @classmethod
     def strip_regex(cls, a_string, regex):
