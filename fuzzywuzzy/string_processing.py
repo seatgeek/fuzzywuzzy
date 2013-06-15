395dc36dde59169c15b6341e8471eb61cd09c565
Olivier Le Thanh Duong <olivier@lethanh.be>
2013-06-15 22:41:41 +0200

    [mod] Don't need all the blah just to redefine method from string module

--- fuzzywuzzy/string_processing.py
+++ fuzzywuzzy/string_processing.py
@@ -17,25 +17,6 @@ class StringProcessor(object):
         """
         return cls.regex.sub(u" ", a_string)
 
-    @classmethod
-    def strip(cls, a_string):
-        """
-        This function strips leading and trailing white space.
-        """
-
-        return a_string.strip()
-
-    @classmethod
-    def to_lower_case(cls, a_string):
-        """
-        This function returns the lower-cased version of the string given.
-        """
-        return a_string.lower()
-
-    @classmethod
-    def to_upper_case(cls, a_string):
-        """
-        This function returns the upper-cased version of the string given.
-        """
-        return a_string.upper()
-
+    strip = staticmethod(string.strip)
+    to_lower_case = staticmethod(string.lower)
+    to_upper_case = staticmethod(string.upper)
