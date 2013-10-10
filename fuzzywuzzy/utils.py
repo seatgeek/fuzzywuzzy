18831965886a96ca0d72be9e0ab59242a1933f96
Grigi <nagrigoriadis@gmail.com>
2013-10-10 12:52:35 +0200

    fixed py2.6

--- fuzzywuzzy/utils.py
+++ fuzzywuzzy/utils.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from fuzzywuzzy.string_processing import StringProcessor
 import sys
-PY3 = sys.version_info.major == 3
+PY3 = sys.version_info[0] == 3
 
 def validate_string(s):
     try:
