59a87a4c9ddde7eeee2af2e206ea81f275209aa4
Jeff Paine <jeffreyalanpaine@gmail.com>
2014-01-19 13:52:34 -0500

    pep8 import ordering

--- fuzzywuzzy/process.py
+++ fuzzywuzzy/process.py
@@ -24,9 +24,9 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-from . import fuzz
-
 import itertools
+
+from . import fuzz
 from . import utils
 
 def extract(query, choices, processor=None, scorer=None, limit=5):
