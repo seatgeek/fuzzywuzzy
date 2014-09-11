759ed35661089563407d823db01d6fe75afe58c9
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-09-11 13:17:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-setup(name='fuzzywuzzy',
-      version='0.3.1',
-      description='Fuzzy string matching in python',
-      long_description=open('README.rst').read(),
-      author='Adam Cohen',
-      author_email='adam@seatgeek.com',
-      url='https://github.com/seatgeek/fuzzywuzzy/',
-      packages=['fuzzywuzzy'],
-      classifiers=(
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3'
-      )
-      )
+# Copyright (c) 2014 SeatGeek
+
+# This file is part of fuzzywuzzy.
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def open_file(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
+setup(
+    name='fuzzywuzzy',
+    version=0.3.1,
+    author='Adam Cohen',
+    author_email='adam@seatgeek.com',
+    packages=['fuzzywuzzy'],
+    url='https://github.com/seatgeek/fuzzywuzzy',
+    license=open('LICENSE.txt').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3'
+    ],
+    description='Fuzzy string matching in python',
+    long_description=open_file('README.rst').read(),
+    zip_safe=True,
+)
