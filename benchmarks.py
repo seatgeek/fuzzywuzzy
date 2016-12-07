a79f3318258e876b2c107048d0f2d325b1e236c1
nolan <nolan@nolankaplan.com>
2016-12-07 04:53:21 -0500

    fix benchmark only generate list once

--- benchmarks.py
+++ benchmarks.py
@@ -90,13 +90,17 @@ for s in cirque_strings:
 
 print 'Test process.exract(scorer =  fuzz.QRatio) for string: "%s"' % s
 print '-------------------------------'
-print_result_from_timeit('process.extract(u\'cirque du soleil\', [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)], scorer =  fuzz.QRatio)',
-                             common_setup + basic_setup + " from fuzzywuzzy import process; import string,random;", number=10)
+print_result_from_timeit('process.extract(u\'cirque du soleil\', choices, scorer =  fuzz.QRatio)',
+                             common_setup + basic_setup + " from fuzzywuzzy import process; import string,random;"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
 
 print 'Test process.exract(scorer =  fuzz.WRatio) for string: "%s"' % s
 print '-------------------------------'
-print_result_from_timeit('process.extract(u\'cirque du soleil\', [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)], scorer =  fuzz.WRatio)',
-                             common_setup + basic_setup + " from fuzzywuzzy import process; import string,random;", number=10)
+print_result_from_timeit('process.extract(u\'cirque du soleil\', choices, scorer =  fuzz.WRatio)',
+                             common_setup + basic_setup + " from fuzzywuzzy import process; import string,random;"
+                             " choices = [\'\'.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30)) for s in range(5000)]",
+                              number=10)
 
 
 # let me show you something
