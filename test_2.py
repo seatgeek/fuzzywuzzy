74d7646a5f48f6a701f6927419412502b2335fde
Hassan24488 <87649507+Hassan24488@users.noreply.github.com>
2023-02-24 23:09:22 +0500

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,78 +1,78 @@
- def testEqual(self):
-        self.assertEqual(fuzz.ratio(self.s1, self.s1a), 100)
-        self.assertEqual(fuzz.ratio(self.s8, self.s8a), 100)
-        self.assertEqual(fuzz.ratio(self.s9, self.s9a), 100)
+def testEqual(self):
+    self.assertEqual(fuzz.ratio(self.s1, self.s1a), 100)
+    self.assertEqual(fuzz.ratio(self.s8, self.s8a), 100)
+    self.assertEqual(fuzz.ratio(self.s9, self.s9a), 100)
 
-    def testCaseInsensitive(self):
-        self.assertNotEqual(fuzz.ratio(self.s1, self.s2), 100)
-        self.assertEqual(fuzz.ratio(utils.full_process(self.s1), utils.full_process(self.s2)), 100)
+def testCaseInsensitive(self):
+    self.assertNotEqual(fuzz.ratio(self.s1, self.s2), 100)
+    self.assertEqual(fuzz.ratio(utils.full_process(self.s1), utils.full_process(self.s2)), 100)
 
-    def testPartialRatio(self):
-        self.assertEqual(fuzz.partial_ratio(self.s1, self.s3), 100)
+def testPartialRatio(self):
+    self.assertEqual(fuzz.partial_ratio(self.s1, self.s3), 100)
 
-    def testTokenSortRatio(self):
-        self.assertEqual(fuzz.token_sort_ratio(self.s1, self.s1a), 100)
+def testTokenSortRatio(self):
+    self.assertEqual(fuzz.token_sort_ratio(self.s1, self.s1a), 100)
 
-    def testPartialTokenSetRatio(self):
-        self.assertEqual(fuzz.partial_token_set_ratio(self.s4, self.s7), 100)
+def testPartialTokenSetRatio(self):
+    self.assertEqual(fuzz.partial_token_set_ratio(self.s4, self.s7), 100)
 
-    def testQuickRatioEqual(self):
-        self.assertEqual(fuzz.QRatio(self.s1, self.s1a), 100)
+def testQuickRatioEqual(self):
+    self.assertEqual(fuzz.QRatio(self.s1, self.s1a), 100)
 
-    def testQuickRatioCaseInsensitive(self):
-        self.assertEqual(fuzz.QRatio(self.s1, self.s2), 100)
+def testQuickRatioCaseInsensitive(self):
+    self.assertEqual(fuzz.QRatio(self.s1, self.s2), 100)
 
-    def testQuickRatioNotEqual(self):
-        self.assertNotEqual(fuzz.QRatio(self.s1, self.s3), 100)
+def testQuickRatioNotEqual(self):
+    self.assertNotEqual(fuzz.QRatio(self.s1, self.s3), 100)
 
-    def testWRatioEqual(self):
-        self.assertEqual(fuzz.WRatio(self.s1, self.s1a), 100)
+def testWRatioEqual(self):
+    self.assertEqual(fuzz.WRatio(self.s1, self.s1a), 100)
 
-    def testWRatioCaseInsensitive(self):
-        self.assertEqual(fuzz.WRatio(self.s1, self.s2), 100)
+def testWRatioCaseInsensitive(self):
+    self.assertEqual(fuzz.WRatio(self.s1, self.s2), 100)
 
-    def testWRatioPartialMatch(self):
-        # a partial match is scaled by .9
-        self.assertEqual(fuzz.WRatio(self.s1, self.s3), 90)
+def testWRatioPartialMatch(self):
+    # a partial match is scaled by .9
+    self.assertEqual(fuzz.WRatio(self.s1, self.s3), 90)
 
-    def testWRatioMisorderedMatch(self):
-        # misordered full matches are scaled by .95
-        self.assertEqual(fuzz.WRatio(self.s4, self.s5), 95)
+def testWRatioMisorderedMatch(self):
+    # misordered full matches are scaled by .95
+    self.assertEqual(fuzz.WRatio(self.s4, self.s5), 95)
 
-    def testWRatioUnicode(self):
-        self.assertEqual(fuzz.WRatio(unicode(self.s1), unicode(self.s1a)), 100)
+def testWRatioUnicode(self):
+    self.assertEqual(fuzz.WRatio(unicode(self.s1), unicode(self.s1a)), 100)
 
-    def testQRatioUnicode(self):
-        self.assertEqual(fuzz.WRatio(unicode(self.s1), unicode(self.s1a)), 100)
+def testQRatioUnicode(self):
+    self.assertEqual(fuzz.WRatio(unicode(self.s1), unicode(self.s1a)), 100)
 
-    def testEmptyStringsScore100(self):
-        self.assertEqual(fuzz.ratio("", ""), 100)
-        self.assertEqual(fuzz.partial_ratio("", ""), 100)
+def testEmptyStringsScore100(self):
+    self.assertEqual(fuzz.ratio("", ""), 100)
+    self.assertEqual(fuzz.partial_ratio("", ""), 100)
 
-    def testIssueSeven(self):
-        s1 = "HSINCHUANG"
-        s2 = "SINJHUAN"
-        s3 = "LSINJHUANG DISTRIC"
-        s4 = "SINJHUANG DISTRICT"
+def testIssueSeven(self):
+    s1 = "HSINCHUANG"
+    s2 = "SINJHUAN"
+    s3 = "LSINJHUANG DISTRIC"
+    s4 = "SINJHUANG DISTRICT"
 
-        self.assertTrue(fuzz.partial_ratio(s1, s2) > 75)
-        self.assertTrue(fuzz.partial_ratio(s1, s3) > 75)
-        self.assertTrue(fuzz.partial_ratio(s1, s4) > 75)
+    self.assertTrue(fuzz.partial_ratio(s1, s2) > 75)
+    self.assertTrue(fuzz.partial_ratio(s1, s3) > 75)
+    self.assertTrue(fuzz.partial_ratio(s1, s4) > 75)
 
-    def testRatioUnicodeString(self):
-        s1 = "\u00C1"
-        s2 = "ABCD"
-        score = fuzz.ratio(s1, s2)
-        self.assertEqual(0, score)
+def testRatioUnicodeString(self):
+    s1 = "\u00C1"
+    s2 = "ABCD"
+    score = fuzz.ratio(s1, s2)
+    self.assertEqual(0, score)
 
-    def testPartialRatioUnicodeString(self):
-        s1 = "\u00C1"
-        s2 = "ABCD"
-        score = fuzz.partial_ratio(s1, s2)
-        self.assertEqual(0, score)
+def testPartialRatioUnicodeString(self):
+    s1 = "\u00C1"
+    s2 = "ABCD"
+    score = fuzz.partial_ratio(s1, s2)
+    self.assertEqual(0, score)
 
-    def testWRatioUnicodeString(self):
-        s1 = "\u00C1"
-        s2 = "ABCD"
-        score = fuzz.WRatio(s1, s2)
-        self.assertEqual(0, score)
+def testWRatioUnicodeString(self):
+    s1 = "\u00C1"
+    s2 = "ABCD"
+    score = fuzz.WRatio(s1, s2)
+    self.assertEqual(0, score)
